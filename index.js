274e1ea7651aab212e6a2e9dcd530450adb3f2a2
Sebastian Polanco <jspolancor@gmail.com>
2017-11-14 22:08:16 -0500

    Adds basic styles, and functions

---
+++ index.js
@@ -0,0 +1,20 @@
+/**
+ * Initializes the puzzle
+ * @param {int} puzzleSize puzzle side size
+ * @returns {array}
+ */
+function initializePuzzle(puzzleSize){
+    return puzzleArray;
+}
+
+function scramblePuzzle(){
+
+}
+
+function moveTile(){
+
+}
+
+function gameOver(){
+
+}
